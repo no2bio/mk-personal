a4777469b296b01579d9661d610acc26572df89f
Michael Bar-Sinai <mich.barsinai@gmail.com>
2015-06-21 16:17:33 +0300

    Call-for-action button stylizes (closes #9). Main issue was to allow MKs with ling names have the button on their card.

--- js/ctrl.js
+++ js/ctrl.js
@@ -140,8 +140,7 @@ function buildMkContent(mk) {
   return "<img class='mk-image' src='img/mks-small/" + mk.alias + ".jpg' alt=''" + mk.name + "'/>\n" +
           "<div class='mk-name'>" + mk.name + "</div>" +
           "<div class='mk-party'>" + mk.party.name + "</div>" +
-          "<div class='mk-contact'><a class='btn btn-sm " +
-          "btn-default" +
+          "<div class='mk-contact'><a class='btn btn-xs btn-act" +
 	  // (mk.status==="y"? "btn-success": mk.status==="n"? "btn-danger": "btn-primary") +
           "' href='mks/" + mk.alias +".html'>" +
 	  (mk.status==="y"? "לתמוך": mk.status==="n"? "למחות": "ללחוץ") +
